e097cf79a701354b7e6660193b3dd9030430d22b
Rémi Dulong <remi.dulong@unine.ch>
2021-06-15 11:34:02 +0000

    improve install + ubuntu mode

--- install.sh
+++ install.sh
@@ -2,4 +2,16 @@
 
 #./install_arch_pakages.sh
 
+./install_ubuntu_packages.sh
+
 stow --dotfiles emacs
+
+
+git config --global user.name "Rémi Dulong"
+git config --global user.email remi.dulong@unine.ch
+
+
+wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O install-zsh.sh
+sh install-zsh.sh
+rm install-zsh.sh
+
